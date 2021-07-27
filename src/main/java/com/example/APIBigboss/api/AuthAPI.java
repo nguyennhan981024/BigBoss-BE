ec90f43b44744fba646f3d4cf71450445d94a274
NhanNguyen <nguyennhan981024@gmail.com>
2021-07-27 21:57:53 +0700

    update api

--- src/main/java/com/example/APIBigboss/api/AuthAPI.java
+++ src/main/java/com/example/APIBigboss/api/AuthAPI.java
@@ -53,7 +53,6 @@ public class AuthAPI {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtils.generateJwtToken(authentication);
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
