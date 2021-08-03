130d5b44396d1a152d351f2c76ba690ee3566c8c
NhanNguyen <nguyennhan981024@gmail.com>
2021-08-04 06:37:16 +0700

    update apo

--- src/main/java/com/example/APIBigboss/api/UserAPI.java
+++ src/main/java/com/example/APIBigboss/api/UserAPI.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -37,8 +38,8 @@ public class UserAPI {
 
 
     @GetMapping //read data
-    public ResponseEntity<Page<User>> getAllUsers(Pageable pageable) {
-        return ResponseEntity.ok(userRepository.getAllUsers(pageable));
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.getAllUsers());
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/example/APIBigboss/repository/UserRepository.java
+++ src/main/java/com/example/APIBigboss/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -20,6 +21,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findUserByID(int userID);
 
     @Query("SELECT u FROM User u")
-    Page<User> getAllUsers(Pageable pageable);
+    List<User> getAllUsers();
 
 }
