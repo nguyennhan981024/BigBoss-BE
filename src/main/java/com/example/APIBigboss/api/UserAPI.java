5ec384162a6b0a3e9a0ab30fc1faca9e3e815a2f
NhanNguyen <nguyennhan981024@gmail.com>
2021-08-05 21:39:37 +0700

    update api

--- src/main/java/com/example/APIBigboss/api/UserAPI.java
+++ src/main/java/com/example/APIBigboss/api/UserAPI.java
@@ -89,6 +89,7 @@ public class UserAPI {
             });
         }
         user.setRoles(roles);
+        user.setPassword(userDTO.getPassword());
         user.setUsername(userDTO.getUsername());
         user.setEmail(userDTO.getEmail());
         user.setUserID(optionalUser.get().getUserID());
