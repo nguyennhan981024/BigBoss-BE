e8d8eeb5d0c4939ed0f2712a0d5874f9f4ae67af
NhanNguyen <nguyennhan981024@gmail.com>
2021-08-05 23:31:31 +0700

    update deploy

--- src/main/java/com/example/APIBigboss/models/Product.java
+++ src/main/java/com/example/APIBigboss/models/Product.java
@@ -17,17 +17,29 @@ public class Product {
     private String status;
     private String img;
     private String cate_name;
+    @Column(columnDefinition = "TEXT")
+    private String size;
+    private String price;
+    @Column(columnDefinition = "TEXT")
+    private String list_img;
+    private String to_rent;
+    private String price_to_rent;
     private int cate_id;
 
     public Product() {
     }
 
-    public Product(int id, String name, String status, String img, String cate_name, int cate_id) {
+    public Product(int id, String name, String status, String img, String cate_name, String size, String price, String list_img, String to_rent, String price_to_rent, int cate_id) {
         this.id = id;
         this.name = name;
         this.status = status;
         this.img = img;
         this.cate_name = cate_name;
+        this.size = size;
+        this.price = price;
+        this.list_img = list_img;
+        this.to_rent = to_rent;
+        this.price_to_rent = price_to_rent;
         this.cate_id = cate_id;
     }
 
@@ -69,6 +81,46 @@ public class Product {
 
     public void setCate_name(String cate_name) {
         this.cate_name = cate_name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getList_img() {
+        return list_img;
+    }
+
+    public void setList_img(String list_img) {
+        this.list_img = list_img;
+    }
+
+    public String getTo_rent() {
+        return to_rent;
+    }
+
+    public void setTo_rent(String to_rent) {
+        this.to_rent = to_rent;
+    }
+
+    public String getPrice_to_rent() {
+        return price_to_rent;
+    }
+
+    public void setPrice_to_rent(String price_to_rent) {
+        this.price_to_rent = price_to_rent;
     }
 
     public int getCate_id() {
